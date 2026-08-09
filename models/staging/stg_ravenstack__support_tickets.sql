13d7999d04cc282dd5013f517efc34ad388048af
Harshal131 <vakharia.harshal18.hv@gmail.com>
2026-08-09 00:47:58 -0400

    Update support ticket model and add test

--- models/staging/stg_ravenstack__support_tickets.sql
+++ models/staging/stg_ravenstack__support_tickets.sql
@@ -1,3 +1,11 @@
-select 
-    *
-from {{ref('ravenstack_support_tickets')}}
+select
+    ticket_id,
+    account_id,
+    cast(submitted_at as date) as submitted_at,
+    cast(closed_at as timestamp) as closed_at,
+    cast(resolution_time_hours as integer) as resolution_time_hours,
+    priority,
+    cast(first_response_time_minutes as integer) as first_response_time_minutes,
+    cast(satisfaction_score as integer) as satisfaction_score,
+    cast(escalation_flag as boolean) as escalation_flag
+from {{ ref('ravenstack_support_tickets') }}
