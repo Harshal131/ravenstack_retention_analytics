455fa8f0b8bf2022cd1ade7fd5dcb59004dc982e
Harshal131 <vakharia.harshal18.hv@gmail.com>
2026-08-09 00:37:02 -0400

    Update subscriptions, feature usage models and add relevant tests

--- models/staging/stg_ravenstack__feature_usage.sql
+++ models/staging/stg_ravenstack__feature_usage.sql
@@ -1,3 +1,10 @@
-select 
-    * 
-from {{ref('ravenstack_feature_usage')}}
+select
+    usage_id,
+    subscription_id,
+    cast(usage_date as date) as usage_date,
+    feature_name,
+    cast(usage_count as integer) as usage_count,
+    cast(usage_duration_secs as integer) as usage_duration_secs,
+    cast(error_count as integer) as error_count,
+    cast(is_beta_feature as boolean) as is_beta_feature
+from {{ ref('ravenstack_feature_usage') }}
--- models/staging/stg_ravenstack__subscriptions.sql
+++ models/staging/stg_ravenstack__subscriptions.sql
@@ -1,3 +1,16 @@
-select 
-    *
-from {{ref('ravenstack_subscriptions')}}
+select
+    subscription_id,
+    account_id,
+    cast(start_date as date) as start_date,
+    cast(end_date as date) as end_date,
+    plan_tier,
+    cast(seats as integer) as seats,
+    cast(mrr_amount as integer) as mrr_amount,
+    cast(arr_amount as integer) as arr_amount,
+    cast(is_trial as boolean) as is_trial,
+    cast(upgrade_flag as boolean) as upgrade_flag,
+    cast(downgrade_flag as boolean) as downgrade_flag,
+    cast(churn_flag as boolean) as churn_flag,
+    billing_frequency,
+    cast(auto_renew_flag as boolean) as auto_renew_flag
+from {{ ref('ravenstack_subscriptions') }}
